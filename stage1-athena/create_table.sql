19dce7e346bf5df319639cbdb3d975e559b38b51
Abdul Rafay Ahmed Khan <137749792+rafayvolga@users.noreply.github.com>
2025-08-05 20:53:18 -0700

    fix(stage1): remove EXTERNAL keyword from CREATE TABLE

---
+++ stage1-athena/create_table.sql
@@ -0,0 +1,33 @@
+-- stage1-athena/create_table.sql
+
+CREATE TABLE IF NOT EXISTS healthcare.facilities (
+  facility_id string,
+  facility_name string,
+  employee_count int,
+  services array<string>,
+  location struct<
+    address:string,
+    city:string,
+    state:string,
+    zip:string
+  >,
+  labs array<
+    struct<
+      lab_name:string,
+      certifications:array<string>
+    >
+  >,
+  accreditation array<
+    struct<
+      accreditation_body:string,
+      accreditation_id:string,
+      valid_until:string
+    >
+  >
+)
+ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+WITH SERDEPROPERTIES (
+  'ignore.malformed.json' = 'true'
+)
+LOCATION 's3://'"rafay-aws-pipeline"'/raw/'
+TBLPROPERTIES ('has_encrypted_data'='false');
