9063498ddfad6a412c35a2c9201dc60b5d4638cb
Abdul Rafay Ahmed Khan <137749792+rafayvolga@users.noreply.github.com>
2025-08-05 20:54:36 -0700

    fix(stage1): collapse nested type defs to single lines

--- stage1-athena/create_table.sql
+++ stage1-athena/create_table.sql
@@ -5,29 +5,15 @@ CREATE TABLE IF NOT EXISTS healthcare.facilities (
   facility_name string,
   employee_count int,
   services array<string>,
-  location struct<
-    address:string,
-    city:string,
-    state:string,
-    zip:string
-  >,
-  labs array<
-    struct<
-      lab_name:string,
-      certifications:array<string>
-    >
-  >,
-  accreditation array<
-    struct<
-      accreditation_body:string,
-      accreditation_id:string,
-      valid_until:string
-    >
-  >
+  location struct<address:string,city:string,state:string,zip:string>,
+  labs array<struct<lab_name:string,certifications:array<string>>>,
+  accreditation array<struct<accreditation_body:string,accreditation_id:string,valid_until:string>>
 )
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 WITH SERDEPROPERTIES (
   'ignore.malformed.json' = 'true'
 )
-LOCATION 's3://'"rafay-aws-pipeline"'/raw/'
-TBLPROPERTIES ('has_encrypted_data'='false');
+LOCATION 's3://'"$BUCKET"'/raw/'
+TBLPROPERTIES (
+  'has_encrypted_data'='false'
+);
