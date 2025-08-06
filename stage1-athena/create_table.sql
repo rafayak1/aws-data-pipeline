1e000534238da446772334e7e2e68ebc585a57d4
Abdul Rafay Ahmed Khan <137749792+rafayvolga@users.noreply.github.com>
2025-08-05 20:55:27 -0700

    fix(stage1): uppercase types/keywords in DDL

--- stage1-athena/create_table.sql
+++ stage1-athena/create_table.sql
@@ -1,19 +1,15 @@
--- stage1-athena/create_table.sql
-
-CREATE TABLE IF NOT EXISTS healthcare.facilities (
-  facility_id string,
-  facility_name string,
-  employee_count int,
-  services array<string>,
-  location struct<address:string,city:string,state:string,zip:string>,
-  labs array<struct<lab_name:string,certifications:array<string>>>,
-  accreditation array<struct<accreditation_body:string,accreditation_id:string,valid_until:string>>
+CREATE EXTERNAL TABLE IF NOT EXISTS healthcare.facilities (
+  facility_id STRING,
+  facility_name STRING,
+  employee_count INT,
+  services ARRAY<STRING>,
+  location STRUCT<address:STRING,city:STRING,state:STRING,zip:STRING>,
+  labs ARRAY<STRUCT<lab_name:STRING,certifications:ARRAY<STRING>>>,
+  accreditation ARRAY<STRUCT<accreditation_body:STRING,accreditation_id:STRING,valid_until:STRING>>
 )
 ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
 WITH SERDEPROPERTIES (
-  'ignore.malformed.json' = 'true'
+  'ignore.malformed.json'='true'
 )
 LOCATION 's3://'"$BUCKET"'/raw/'
-TBLPROPERTIES (
-  'has_encrypted_data'='false'
-);
+TBLPROPERTIES ('has_encrypted_data'='false');
